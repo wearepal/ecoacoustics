44a687b4a688c5559732a02d8bfc692c20b5d580
DavidHurst <davidshrst@gmail.com>
2021-09-16 17:08:20 +0100

    Preparation for training random forest.

--- ecoacoustics/Ecoacoustics Script.py
+++ ecoacoustics/Ecoacoustics Script.py
@@ -1,12 +1,14 @@
 from pathlib import Path
 import torch
 from conduit.data.datamodules.audio import EcoacousticsDataModule
+from torch.utils.data import Dataset
+from sklearn.ensemble import RandomForestClassifier
 import numpy as np
 from tqdm import tqdm
 
 # Initialise a datamodule for testing.
 root_dir = Path().resolve().parent.parent
-dm = EcoacousticsDataModule(root_dir, specgram_segment_len=1, test_prop=0, val_prop=0)
+dm = EcoacousticsDataModule(root_dir, specgram_segment_len=1, test_prop=0, val_prop=0, train_batch_size=1)
 dm.prepare_data()
 dm.setup()
 
@@ -23,13 +25,23 @@ model = torch.hub.load('DavidHurst/torchvggish', 'vggish', preprocess=False, dev
 # Test loop.
 model.eval()
 preds = []
-for data in tqdm(enumerate(data_loader)):
-    x = data.x.to(device)
-    y = model(x)
-    preds.append(y.detach.cpu().numpy())
+i = 0
+with torch.no_grad():
+    for data in tqdm(data_loader, desc="Predicting"):
+        x = data.x.to(device)
+        y = model(x)
+        preds.append(y.cpu().numpy())
+        i += 1
+        if i > 100:
+            break
 
-preds = np.concat(preds, axis=0)
+print(f'Prediction shape: {preds[0].size()}')
+print('Values of prediction 0:', preds[0])
+np.savetxt(root_dir / "preds.csv", np.asarray(preds), delimiter=",")
 
-print(len(preds))
-print(preds[0])
+class PredictionDataset(Dataset):
+    
+
+rm_clf = RandomForestClassifier()
+
 
