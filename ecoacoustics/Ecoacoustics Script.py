edef889e66bda30d7052fdd24cbc868caf07d635
DavidHurst <davidshrst@gmail.com>
2021-09-20 13:12:43 +0100

    Tidy up.

--- ecoacoustics/Ecoacoustics Script.py
+++ ecoacoustics/Ecoacoustics Script.py
@@ -4,15 +4,16 @@ from conduit.data.datamodules.audio import EcoacousticsDataModule
 from sklearn import ensemble, model_selection, tree, metrics
 from tqdm import tqdm
 import numpy as np
+import matplotlib.pyplot as plt
 
 
-# Initialise a datamodule for testing.
+# Initialise a datamodule (stores dataloaders).
 root_dir = Path().resolve().parent.parent
 dm = EcoacousticsDataModule(root_dir, specgram_segment_len=1, test_prop=0, val_prop=0, train_batch_size=1)
 dm.prepare_data()
 dm.setup()
 
-print(f'The train/test/validation split of the dataset is:\n  {dm.num_train_samples}/{dm.num_test_samples}/{dm.num_val_samples}.')
+print(f'The train/test/validation split of the dataset is: {dm.num_train_samples}/{dm.num_test_samples}/{dm.num_val_samples}.')
 
 # Get data loader, all data.
 data_loader = dm.train_dataloader()
@@ -20,45 +21,44 @@ data_loader = dm.train_dataloader()
 # Define device.
 device = torch.device("cuda:1" if torch.cuda.is_available() else "cpu")
 
-# Load VGGish.
+# Load pre-trained VGGish.
 model = torch.hub.load('DavidHurst/torchvggish', 'vggish', preprocess=False, device=device, postprocess=False)
 
 # Test loop.
 model.eval()
-preds = []
-labels = []
-i = 0
+preds_ls = []
+labels_ls = []
 with torch.no_grad():
     for data in tqdm(data_loader, desc="Generating Representations"):
         x = data.x.to(device)
         y = data.y.to(device)
-
         pred = model(x)
-        preds.append(pred.cpu().numpy())
-        labels.append(y.cpu().numpy())
+        preds_ls.append(pred.cpu().numpy())
+        labels_ls.append(y.cpu().numpy())
 
-        i += 1
-        if i > 10_000:
-            break
+preds = np.concatenate(preds_ls)
+labels = np.concatenate(labels_ls)
 
-preds = [p.squeeze() for p in preds]
-labels = np.asarray(labels).ravel()
-
-# Save memory, del spectrogram encoder.
+# Save memory, delete spectrogram encoder as it's no longer needed.
 del model
 
-print(f'Shape of first sample = {preds[0].shape}')
-print(f'Label 1 = {labels[0]}, shape = {labels[0].shape}')
-            
 # Split data into train and test for random forest.
 X_train, X_test, y_train, y_test = model_selection.train_test_split(preds, labels, test_size=0.20)
 
 # Fit random forest to data and get predictions.
-rm_clf = ensemble.RandomForestClassifier()
-rm_clf.fit(X_train, y_train)
-y_test_pred = rm_clf.predict(X_test)
+rf_clf = ensemble.RandomForestClassifier()
+rf_clf.fit(X_train, y_train)
+y_pred = rf_clf.predict(X_test)
 
 # Evaluate classifier's performance.
-f1 = metrics.f1_score(y_test, y_test_pred, average='micro')
+f1 = metrics.f1_score(y_test, y_pred, average='micro')
+cm = metrics.confusion_matrix(y_test, y_pred)
 
-print(f'The F1 score for the predictions is: {f1:.2f}')
+print(f'Classes: {rf_clf.classes_}.')
+
+print(f'The F1 score for the predictions is: {f1:.2f}%.')
+print(f'Confusion matrix: \n{cm}')
+
+fig, ax = plt.subplots()
+metrics.ConfusionMatrixDisplay(cm).plot(ax=ax)
+fig.savefig(root_dir / "random_forest_cm.png")
