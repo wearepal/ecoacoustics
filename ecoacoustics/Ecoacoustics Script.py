ebc2dc421d802a45602a0224e7b846f8e603c368
DavidHurst <davidshrst@gmail.com>
2021-09-21 11:03:06 +0100

    Added entry point.

--- ecoacoustics/Ecoacoustics Script.py
+++ ecoacoustics/Ecoacoustics Script.py
@@ -1,3 +1,10 @@
+#!/usr/bin/env python3
+
+"""
+    A script to demonstrate the Conduit EcoacousticsDataModule, using it to generate some 
+    representations of the audio samples and clasisfying them with a random forest.
+"""
+
 from pathlib import Path
 import torch
 from conduit.data.datamodules.audio import EcoacousticsDataModule
@@ -7,58 +14,69 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-# Initialise a datamodule (stores dataloaders).
-root_dir = Path().resolve().parent.parent
-dm = EcoacousticsDataModule(root_dir, specgram_segment_len=1, test_prop=0, val_prop=0, train_batch_size=1)
-dm.prepare_data()
-dm.setup()
+def main():
+    # Initialise a datamodule (stores dataloaders).
+    root_dir = Path().resolve().parent.parent
+    dm = EcoacousticsDataModule(root_dir, specgram_segment_len=1, test_prop=0, val_prop=0, train_batch_size=1, num_freq_bins=1024)
+    dm.prepare_data()
+    dm.setup()
 
-print(f'The train/test/validation split of the dataset is: {dm.num_train_samples}/{dm.num_test_samples}/{dm.num_val_samples}.')
+    print(f'The train/test/validation split of the dataset is: {dm.num_train_samples}/{dm.num_test_samples}/{dm.num_val_samples}.')
 
-# Get data loader, all data.
-data_loader = dm.train_dataloader()
+    # Get data loader, all data.
+    data_loader = dm.train_dataloader()
 
-# Define device.
-device = torch.device("cuda:1" if torch.cuda.is_available() else "cpu")
+    # Define device.
+    device = torch.device('cpu') #"cuda:1" if torch.cuda.is_available() else "cpu")
 
-# Load pre-trained VGGish.
-model = torch.hub.load('DavidHurst/torchvggish', 'vggish', preprocess=False, device=device, postprocess=False)
+    # Load pre-trained VGGish.
+    model = torch.hub.load('DavidHurst/torchvggish', 'vggish', preprocess=False, device=device, postprocess=False)
 
-# Test loop.
-model.eval()
-preds_ls = []
-labels_ls = []
-with torch.no_grad():
-    for data in tqdm(data_loader, desc="Generating Representations"):
-        x = data.x.to(device)
-        y = data.y.to(device)
-        pred = model(x)
-        preds_ls.append(pred.cpu().numpy())
-        labels_ls.append(y.cpu().numpy())
+    # Test loop.
+    model.eval()
+    preds_ls = []
+    labels_ls = []
+    i = 0
+    with torch.no_grad():
+        for data in tqdm(data_loader, desc="Generating Representations"):
+            x = data.x.to(device)
+            y = data.y.to(device)
+            pred = model(x)
+            preds_ls.append(pred.cpu().numpy())
+            labels_ls.append(y.cpu().numpy())
+            
+            if i > 100:
+                break
+            i += 1
 
-preds = np.concatenate(preds_ls)
-labels = np.concatenate(labels_ls)
+    preds = np.concatenate(preds_ls)
+    labels = np.concatenate(labels_ls)
+    
+    
 
-# Save memory, delete spectrogram encoder as it's no longer needed.
-del model
+    # Save memory, delete spectrogram encoder as it's no longer needed.
+    del model
 
-# Split data into train and test for random forest.
-X_train, X_test, y_train, y_test = model_selection.train_test_split(preds, labels, test_size=0.20)
+    # Split data into train and test for random forest.
+    X_train, X_test, y_train, y_test = model_selection.train_test_split(preds, labels, test_size=0.20)
 
-# Fit random forest to data and get predictions.
-rf_clf = ensemble.RandomForestClassifier()
-rf_clf.fit(X_train, y_train)
-y_pred = rf_clf.predict(X_test)
+    # Fit random forest to data and get predictions.
+    rf_clf = ensemble.RandomForestClassifier()
+    rf_clf.fit(X_train, y_train)
+    y_pred = rf_clf.predict(X_test)
 
-# Evaluate classifier's performance.
-f1 = metrics.f1_score(y_test, y_pred, average='micro')
-cm = metrics.confusion_matrix(y_test, y_pred)
+    # Evaluate classifier's performance.
+    f1 = metrics.f1_score(y_test, y_pred, average='micro')
+    cm = metrics.confusion_matrix(y_test, y_pred)
 
-print(f'Classes: {rf_clf.classes_}.')
+    print(f'Classes: {rf_clf.classes_}.')
 
-print(f'The F1 score for the predictions is: {f1:.2f}%.')
-print(f'Confusion matrix: \n{cm}')
+    print(f'The F1 score for the predictions is: {f1:.2f}%.')
+    print(f'Confusion matrix: \n{cm}')
 
-fig, ax = plt.subplots()
-metrics.ConfusionMatrixDisplay(cm).plot(ax=ax)
-fig.savefig(root_dir / "random_forest_cm.png")
+    fig, ax = plt.subplots()
+    metrics.ConfusionMatrixDisplay(cm).plot(ax=ax)
+    fig.savefig(root_dir / "random_forest_cm.png")
+
+if __name__ == '__main__':
+    main()
